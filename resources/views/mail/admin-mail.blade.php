a16505268c5ae6d0d5501fc041f0dfb030f8b788
Louise Devolder <louise.devolder@onlyhumans.com>
2024-02-09 14:25:11 +0000

    add basic styling for mail template

--- resources/views/mail/admin-mail.blade.php
+++ resources/views/mail/admin-mail.blade.php
@@ -4,11 +4,22 @@
     <tbody>
         @foreach($data as $label => $value)
             <tr>
-                <td>{{ $label }}</td>
-                <td>{{ $value }}</td>
+                <td
+                    valign="top"
+                    style="font-weight: 700; padding-bottom: 5px;"
+                >
+                    {{ $label }}
+                </td>
+                <td
+                    valign="top"
+                    style="padding-bottom: 5px; padding-left: 30px;"
+                >
+                    {{ $value }}
+                </td>
             </tr>
         @endforeach
     </tbody>
 </table>
-
-<a href="{{ $url }}" target="_blank">View in CMS</a>
+<div style="padding-top: 30px;">
+    <a href="{{ $url }}" target="_blank">View in CMS</a>
+</div>
